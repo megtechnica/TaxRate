dd3b8ecca9acd6fe85c211ff492f4f00781206de
Megan K Carroll <101686829+megtechnica@users.noreply.github.com>
2024-10-05 19:41:37 -0600

    Add files via upload

--- Module_3/src/TaxRate/TaxRate.java
+++ Module_3/src/TaxRate/TaxRate.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class TaxRate {
 
 	public static void main(String [] args) { 
+		
+		// instantiate variables
 	      Scanner scnr = new Scanner(System.in);
 	      double hourlyWage;
 	      int weeklyHours;
@@ -11,15 +13,21 @@ public class TaxRate {
 	      double taxWithHolding;
 	      double takeHomePay;
 	      final int WEEKLY_LIMIT = 40;
+	      
+	      // prompt the user for their hourly wage
 	   
 	      System.out.println("Enter hourly wage: ");
 	      hourlyWage = scnr.nextDouble();
+	      
+	      // prompt the user for how many hours they worked this week
 
 	      System.out.println("Enter how many hours you worked this week: ");
 	      weeklyHours = scnr.nextInt();
 	      
+	      // find the gross weekly salary
 	      grossWeeklySalary = hourlyWage * weeklyHours;
 	      
+	      // determine the tax rate
 	      if (grossWeeklySalary < 500) {
 	    	  taxRate = 0.10;
 	      }
@@ -32,9 +40,11 @@ public class TaxRate {
 	      else {
 	    	  taxRate = 0.30;
 	      }
+	      // calculate the tax withholding and take home pay
 	      taxWithHolding = grossWeeklySalary * taxRate;
 	      takeHomePay = grossWeeklySalary - taxWithHolding;
-
+	      
+	      // output 
 	      System.out.println("Gross weekly salary: " + grossWeeklySalary);
 	      System.out.println("Tax Withholding: " + taxWithHolding);
 	      System.out.println("Takehome Pay: " + takeHomePay);      
